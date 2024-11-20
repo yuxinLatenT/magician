1480d64bcb86a1cee5b1b7040183aedd1a1f4ce8
yuxin1202 <ninjawyx@gmail.com>
2024-11-21 01:49:12 +0800

    button to link

---
+++ main.js
@@ -0,0 +1,7 @@
+function home_btn_onclick(element){
+    window.location = ""; //首頁連結
+}
+
+function github_btn_onclick(element){
+    window.location = "https://github.com/yuxinLatenT/magician";
+}
