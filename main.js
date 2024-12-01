cfa5dbaf1007a65437e68d4c845de7f714380b3f
yuxin1202 <ninjawyx@gmail.com>
2024-12-01 15:56:51 +0800

    darkmode set done

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ function home_btn_onclick(element){
 function github_btn_onclick(element){
     window.location = "https://github.com/yuxinLatenT/magician";
 }
+
+const darkmode_toggle = document.getElementById('darkmode_toggle');
+
+darkmode_toggle.addEventListener("change", function(){
+    if(this.checked){
+        document.body.classList.add("dark_mode");
+    }
+    else{
+        document.body.classList.remove("dark_mode");
+    }
+});
