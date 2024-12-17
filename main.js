25456444c49dcbdcaeb1fdcc8579486a3a4c9194
yuxin1202 <ninjawyx@gmail.com>
2024-12-17 12:43:44 +0800

    增加遊戲開始按鈕，還有元素是否顯示在畫面上的設定

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function home_btn_onclick(element){
-    window.location = ""; //首頁連結
+    window.location = "index.html"; //首頁連結
 }
 
 function github_btn_onclick(element){
@@ -13,6 +13,18 @@ function mem_card_onclick(element){
 function type_text_onclick(element){
     window.location.href = "type_text.html";
 }
+
+document.body.classList.remove("mem_game_start");
+
+// 遊戲是否開始以及行動
+const mem_start = document.getElementById("mem_start_btn");
+
+mem_start.addEventListener("click", function(){
+    let introduction = document.getElementById("intro");
+    introduction.remove();
+
+    document.body.classList.add("mem_game_start");
+});
 
 // 黑暗模式設定
 const darkmode_toggle = document.getElementById("darkmode_toggle");
